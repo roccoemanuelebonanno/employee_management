b9cca935ece46fffca2197f6bed834e6ea7bf5cc
Rocco Emanuele Bonanno <emanuelebonanno@192.168.1.59>
2024-12-02 00:08:02 +0100

    rollback: Employee scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_28_222146) do
+ActiveRecord::Schema[8.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "employees", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "tax_code"
-    t.date "age"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
